b6bbd9faa5c71d9052a1c634b01241ab52541f9a
Adrien <adrienba63@gmail.com>
2024-09-05 00:45:36 +0200

    Bouton retour, nouveau theme, ameliorations graphiques

--- static/acceuil.js
+++ static/acceuil.js
@@ -3,3 +3,16 @@ function Redirection1() {
 };
 
 
+const classic_button = document.getElementById("Bouton1");
+const qcm_button = document.getElementById("Bouton2");
+
+classic_button.addEventListener('click', function () {
+    window.location.href = "/hello"
+});
+
+
+
+
+
+classic_button.style.backgroundColor = 'green';
+qcm_button.style.backgroundColor = 'red';
