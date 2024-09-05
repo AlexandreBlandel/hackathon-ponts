33e1d3f9f1a1af78465a4fc1587e2a3801c5a4d3
Adrien <adrienba63@gmail.com>
2024-09-05 15:15:25 +0200

    Reprise modif pdf

--- main.py
+++ main.py
@@ -71,15 +71,13 @@ def update_qcm():
         ask_question_to_pdf.update_button(button)
         return dic
 
+
 @app.route("/solution_qcm")
 def solution_qcm():
     return render_template("reponse_qcm.html")
 
 
-@app.route("/get_aide_text")
-    return {}
-
 @app.route("/choix_pdf", methods=["GET", "POST"])
 def choice_PDF():
-    # ask_question_to_pdf.change_PDF()
-    return render_template("change.html")
+    ask_question_to_pdf.change_PDF()
+    return None
